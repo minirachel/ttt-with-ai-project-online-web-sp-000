1de69aa78d3247e069a47eaee31763be9d7e4eab
minirachel <rachel.a.levinson@gmail.com>
2021-03-31 18:36:18 -0400

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,49 @@
+require 'pry'
+
+class Board
+
+    attr_accessor :cells, :token
+
+    def initialize
+        @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
+    def reset!
+        @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
+    def display
+        puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+        puts "-----------"
+        puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+        puts "-----------"
+        puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+    end
+
+    def position(input)
+        pos = input.to_i - 1
+        # binding.pry
+        @cells[pos]
+    end
+
+    def full?
+        @cells.none?(" ")
+    end
+
+    def turn_count
+        @cells.count {|c| c != " "}
+    end
+
+    def taken?(input)
+        position(input) == "X" || position(input) == "O"
+    end
+
+    def valid_move?(input)
+        !taken?(input) && input.to_i.between?(1,9)
+    end
+
+    def update(input, player)
+        @cells[input.to_i - 1] = player.token
+    end
+
+end
